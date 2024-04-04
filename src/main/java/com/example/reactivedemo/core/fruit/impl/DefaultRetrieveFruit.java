1c99b20d8db6cd6b7969f6b93b08b899a36dbb0c
choco9620 <esteban9620@gmail.com>
2024-04-04 09:15:22 -0500

    commit test

--- src/main/java/com/example/reactivedemo/core/fruit/impl/DefaultRetrieveFruit.java
+++ src/main/java/com/example/reactivedemo/core/fruit/impl/DefaultRetrieveFruit.java
@@ -12,16 +12,27 @@ import com.example.reactivedemo.repository.domain.FruitEntity;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 
+/**
+ * esto es un commnet
+ */
 @Service
 @RequiredArgsConstructor
 public class DefaultRetrieveFruit implements RetrieveFruit {
 
+    // holi
     private final FruitRepository fruitRepository;
 
     @Override
     public Flux<Fruit> get() {
+        System.out.println("hola mundo");
         List<FruitEntity> fruitList = fruitRepository.findAll();
+        System.out.println(String.format("esto es una prueba de conflicto tamanio %s", fruitList.size()));
 
+        String cadena = """
+                hola mundo
+                            vdsaa
+                    dfghb
+                """;
         return Flux.fromIterable(fruitList.stream().map(f -> Fruit.builder()
                 .id(f.getId())
                 .name(f.getName())
